25a36b9119e0ff4646de0ec63f174f7e44c62096
RamonsArchive <rmcdeem.m@gmail.com>
2025-10-09 14:44:59 -0700

    added music

---
+++ src/music.ts
@@ -0,0 +1,10 @@
+const listOfSongs = ["nail polish", "till im dead", "collapse", "soil", "I think they call this love"];
+
+export const getSongs = () => {
+    for (const song of listOfSongs) {
+        console.log(song);
+    }
+}
+
+getSongs();
+
