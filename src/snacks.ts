c28ad99dbcd0187d15303fe6a1e670746ad20d16
pleyvaramirez <156385234+pleyvaramirez@users.noreply.github.com>
2025-10-09 14:53:37 -0700

    added Phillip's snacks + chips

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,3 +1,3 @@
-let cookies: string[] = ["cookies", "cheezits", "grapes", "mangoes", "ice cream", "brownies"];
+let cookies: string[] = ["cookies", "cheezits", "grapes", "mangoes", "ice cream", "brownies", "chips"];
 
 console.log(cookies);
