61fe067ab2be8718c25312fbff53de640d18d328
MoisesRM32 <141669746+MoisesRM32@users.noreply.github.com>
2025-10-09 15:26:03 -0700

    sadMusic

---
+++ src/sadMusic.ts
@@ -0,0 +1,9 @@
+const sadmusic = ["Wish","Jocelyn Flores","Robbery"]
+
+function playSadMusic(items: string[])  {
+    items.forEach(item => console.log(item))
+    return;
+}
+
+playSadMusic(sadmusic);
+
