f2810b041cbbf9805531ce476429fbd15a2c2f18
Cheung Cheng <156318983+Cheung0330@users.noreply.github.com>
2025-10-09 15:25:30 -0700

    feat: main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+<<<<<<< HEAD
 import { printSnacks } from "./snacks";
 
 function main(): void {
   printSnacks(); // call the feature from snacks.ts
   console.log("Enjoy your snacks!");
+=======
+import { printSnacks } from "./snacks"; 
+
+function main() {
+    console.log("--- Starting the Ultimate Party Plan ---");
+    
+    printSnacks(); 
+
+    console.log("--- Party Plan Execution Complete ---");
+>>>>>>> f0cada5 (feat: main)
 }
 
 main();
