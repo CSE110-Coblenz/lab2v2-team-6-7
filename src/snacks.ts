7d54de22145dcb063d725e476bdc18ce750d4911
Satvik Bandi <satvikcbandi@gmail.com>
2025-10-09 16:06:04 -0700

    made the animation for snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,9 @@
+import { playAnimation } from './animation'; // <-- 1. ADD THIS IMPORT
+
 const snacks: string[] = ["apples", "bananas", "chips", "gum", "cheetos"]
 
 export const writesnacks = (): void => {
+    playAnimation("Snacks");
     console.log('Snacks:');
     snacks.forEach(snack => {console.log(snack);});
     };
