041954aa6daa4915ec4789b0381c8072a922accf
Ansh Patel <avp003@ucsd.edu>
2025-10-09 17:10:31 -0700

    Added animation.ts and integrated animation pt2

---
+++ src/animation.ts
@@ -0,0 +1,4 @@
+export function animate(feature: string): void {
+    console.log(`\x1b[1mParty! Party! Party! - ${feature} Time\x1b[0m`);
+    console.log(`\x1b[3mLet’s jam before we print the feature!\x1b[0m`);
+  }
