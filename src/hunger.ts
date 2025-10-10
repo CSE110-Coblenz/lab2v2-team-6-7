dfea17633cae9475739cac44a7ce4323a930e16d
Ansh Patel <avp003@ucsd.edu>
2025-10-09 17:10:31 -0700

    Added animation.ts and integrated animation pt2

--- src/hunger.ts
+++ src/hunger.ts
@@ -1,8 +1,8 @@
-
+import { animate } from "./animation";
 const foods: string[] = ['ramen', 'gyozas', 'butter chicken', 'mutton biryani', 'pani puri'];
 
 export function bhukh(): void{
-   
+    animate("Hunger");
     console.log('Food that I crave when hungry: ');
     for (const food of foods){
         console.log("-" + food);
