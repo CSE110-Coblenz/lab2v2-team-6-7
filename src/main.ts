015b58db99aa3b2a4b3af7d1a2b77859e213e4ea
yas033 <yas033@ucsd.edu>
2025-10-11 20:19:28 -0700

    main: call printSnacks()

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-// src/main.ts
 import { printMusic } from "./music";
+
 function main() {
-  printMusic();          //make the conflict here to practice git merge
+  printMusic(); // conflict demo
 }
+
 main();
